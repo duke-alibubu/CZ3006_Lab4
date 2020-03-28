0a92ea977574b047ec3c26940d1230b0975232db
duke-alibubu <ttdungalibubu@gmail.com>
2020-03-28 23:11:49 +0800

    ques 2

--- lab4.py
+++ lab4.py
@@ -5,4 +5,7 @@ import matplotlib.pyplot as plt
 traffic_data = pd.read_csv('data.csv',header = None)
 
 # 1. Top 5 senders
-print traffic_data[9].value_counts().nlargest(5)
+top_5_senders = traffic_data[9].value_counts().nlargest(5)
+
+# 2. Top 5 receivers
+top_5_receivers = traffic_data[10].value_counts().nlargest(5)
